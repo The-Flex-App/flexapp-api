8f9d69fefa5c5a2d89e123a11883f5c72a698a92
Anuj Gakhar <anuj.gakhar@gmail.com>
2020-08-25 20:14:24 +0100

    remove ssl

--- src/app.js
+++ src/app.js
@@ -13,8 +13,8 @@ import fs from 'fs';
 
 const configurations = {
   // Note: You may need sudo to run on port 443
-  production: { ssl: true, port: 8080, hostname: '3.8.172.18' },
-  development: { ssl: true, port: 8080, hostname: 'localhost' },
+  production: { ssl: false, port: 8080, hostname: '3.8.172.18' },
+  development: { ssl: false, port: 8080, hostname: 'localhost' },
 };
 
 const environment = process.env.NODE_ENV || 'production';
