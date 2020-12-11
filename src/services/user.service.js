0f37b956baf9dfb99e9df9834e5a3ea874b551b6
Yashwant Singh Zala <yashwantsinghzala@gmail.com>
2020-12-11 13:50:39 +0530

    invite flow and validation with create user

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,9 +1,10 @@
-import BaseService from './base.service';
-import { userWorkspaceService } from './userworkspace.service'
-import User from '../models/user.model';
-import { compare, hash } from 'bcrypt';
-import { transaction } from 'objection';
-import { UserInputError } from 'apollo-server-express';
+import BaseService from "./base.service";
+import { userWorkspaceService } from "./userworkspace.service";
+import User from "../models/user.model";
+import { compare, hash } from "bcrypt";
+import { transaction } from "objection";
+import { UserInputError } from "apollo-server-express";
+import { invitationService } from "./invitation.service";
 
 const HASH_ROUNDS = 12;
 
@@ -29,39 +30,71 @@ class UserService extends BaseService {
   }
 
   async getUserInfo(userid) {
-    const ownerInfo = await User.query().findOne('id', userid);
+    const ownerInfo = await User.query().findOne("id", userid);
     const workspaceId = ownerInfo.workspaceId;
 
-    const ownerWorkspaceInfo = await User.query().select('users.id', 'users.first_name', 'users.last_name', 'users.email'
-      , 'userworkspace.role', 'users.user_name')
-      .innerJoin('users_workspace as userworkspace', 'users.id', 'userworkspace.user_id')
-      .where('userworkspace.workspace_id', workspaceId)
-      .where('userworkspace.role', 'member')
+    const ownerWorkspaceInfo = await User.query()
+      .select(
+        "users.id",
+        "users.first_name",
+        "users.last_name",
+        "users.email",
+        "userworkspace.role",
+        "users.user_name"
+      )
+      .innerJoin(
+        "users_workspace as userworkspace",
+        "users.id",
+        "userworkspace.user_id"
+      )
+      .where("userworkspace.workspace_id", workspaceId)
+      .where("userworkspace.role", "member");
 
-    const memberWorkspaceInfo = await User.query().select('users.id', 'users.first_name', 'users.last_name', 'users.email',
-      'userworkspace.role', 'userworkspace.user_id', 'userworkspace.workspace_id')
-      .innerJoin('users_workspace as userworkspace', 'users.workspace_id', 'userworkspace.workspace_id')
-      .where('userworkspace.user_id', userid)
-      .where('userworkspace.role', 'member')
+    const memberWorkspaceInfo = await User.query()
+      .select(
+        "users.id",
+        "users.first_name",
+        "users.last_name",
+        "users.email",
+        "userworkspace.role",
+        "userworkspace.user_id",
+        "userworkspace.workspace_id"
+      )
+      .innerJoin(
+        "users_workspace as userworkspace",
+        "users.workspace_id",
+        "userworkspace.workspace_id"
+      )
+      .where("userworkspace.user_id", userid)
+      .where("userworkspace.role", "member");
 
-    const userInfo = { ...ownerInfo, ownerWorkspaceInfo, memberWorkspaceInfo }
+    const userInfo = { ...ownerInfo, ownerWorkspaceInfo, memberWorkspaceInfo };
 
     return userInfo;
-
   }
-
 
   async createUser(input) {
     let trx;
     try {
       trx = await transaction.start(User.knex());
+      const { id, workspaceId, inviteId } = input;
+      const existingUser = this.findById(id);
       const userId = input.id;
-      const workspaceId = 'ws' + userId
-      input.workspaceId = workspaceId;
-      await User.query(trx).insert(input);
-      const role = 'owner';
-      const workspaceinput = { userId, workspaceId, role };
-      await userWorkspaceService.createUserWorkspace(workspaceinput, trx);
+      // invitation flow for a member user
+      if (existingUser && workspaceId && inviteId) {
+        const workspaceinput = { userId, workspaceId, role: "member" };
+        const invitationDetails = { id: parseInt(inviteId), workspaceId };
+        await userWorkspaceService.createUserWorkspace(workspaceinput, trx);
+        await invitationService.editInvite(invitationDetails, trx);
+      } else {
+        // normal user flow
+        const userWorkspaceId = "ws" + userId;
+        input.workspaceId = userWorkspaceId;
+        await User.query(trx).insert(input);
+        const role = "owner";
+        const workspaceinput = { userId, workspaceId: userWorkspaceId, role };
+        await userWorkspaceService.createUserWorkspace(workspaceinput, trx);
+      }
       await trx.commit();
       const userInfo = await this.getUserInfo(userId);
       return userInfo;
@@ -83,7 +116,7 @@ class UserService extends BaseService {
       const user = await this.findByEmail(editUserReq.email);
 
       if (user && user.id !== id) {
-        throw new UserInputError('Email address exists!');
+        throw new UserInputError("Email address exists!");
       }
     }
 
@@ -98,6 +131,15 @@ class UserService extends BaseService {
     await User.query().deleteById(id);
 
     return user;
+  }
+
+  async isValidateUser(userId, workspaceId) {
+    const user = await this.findById(userId);
+    console.log(user);
+    if (user.workspaceId === workspaceId) {
+      return true;
+    }
+    return false;
   }
 
   async changePassword(id, password, newPassword) {
@@ -117,11 +159,11 @@ class UserService extends BaseService {
   }
 
   async findByEmail(email) {
-    return User.query().findOne('email', email);
+    return User.query().findOne("email", email);
   }
 
   async findById(id) {
-    return User.query().findOne('id', id);
+    return User.query().findOne("id", id);
   }
 }
 
